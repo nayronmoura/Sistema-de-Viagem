cc70ebbc85341d0c0e0b2b1ddaec7f91194d718d
NaJulaa <99447462+NaJulaa@users.noreply.github.com>
2022-05-09 23:29:38 -0300

    Add files via upload

--- Veiculo/veiculoBanco.py
+++ Veiculo/veiculoBanco.py
@@ -1,3 +1,56 @@
+<<<<<<< HEAD
+import json
+
+
+def bancoveiculo():
+    with open('bancoveiculo.json', 'r') as fp:
+        dados = json.load(fp)
+        return dados
+
+
+def checkPlaca(placa):
+    banco = bancoveiculo()
+    if placa in banco:
+        return True
+
+
+def cadastro(veiculo):
+    banco = bancoveiculo()
+    banco[veiculo.get('placa')] = veiculo
+    with open('bancoveiculo.json', 'w') as fp:
+        json.dump(dict(banco), fp, indent=4)
+
+
+def buscarPlaca(placa):
+    banco = bancoveiculo()
+    return banco.get(placa)
+
+def motoristaVeiculo(placa,motorista):
+    banco = bancoveiculo()
+    veiculo = banco.get(placa)
+    veiculo['motorista'] = motorista
+    banco[placa] = veiculo
+    with open('bancoveiculo.json', 'w') as fp:
+        json.dump(dict(banco),fp, indent=4)
+
+
+def removerMotorista(placa):
+    banco = bancoveiculo()
+    veiculo = banco.get(placa)
+    veiculo['motorista'] = ''
+    banco[placa] = veiculo
+    with open('bancoveiculo.json','w') as fp:
+        json.dump(dict(banco),fp, indent=4)
+
+
+def removerVeiculo(placa):
+    banco = bancoveiculo()
+    banco.pop(placa)
+    with open('bancoveiculo.json','w') as fp:
+        json.dump(dict(banco),fp, indent=4)
+
+
+=======
 import json
 
 
@@ -35,3 +88,4 @@ def buscarPlaca(placa):
         return True
 
 
+>>>>>>> 8a8063cdedaad4309bea0e1ffcacc058cf5bd1cf
--- Veiculo/veiculoController.py
+++ Veiculo/veiculoController.py
@@ -1,3 +1,127 @@
+<<<<<<< HEAD
+import veiculoBanco
+
+banco = veiculoBanco
+
+
+def main():
+    chose = int(input(" Escolha uma opção: "))
+    match chose:
+        case 1:
+            cadastrar()
+        case 2:
+            buscarPlaca()
+        case 3:
+            motoristaVeiculo()
+        case 4:
+            removerMotorista()
+        case 5:
+            veiculosEmotoristas()
+        case 6:
+            motoristasSveiculos()
+        case 7:
+            removerVeiculo()
+
+
+def cadastrar():
+    while True:
+        placa = str(input('Digite a placa que quer cadastrar: '))
+        if banco.checkPlaca(placa):
+            print('A placa já está cadastrada!! ')
+        else:
+            break
+    while True:
+        tipo = int(input('é: [1] Carro    [2] Moto'))
+        if tipo == 2:
+            veiculo = 'moto'
+            break
+        elif tipo == 1:
+            veiculo = 'carro'
+            break
+        else:
+            print('O valor não é válido !!')
+    banco.cadastro({'placa': placa, 'tipo': veiculo, 'motorista': ''})
+
+
+def motorista():
+    while True:
+        motorista = str(input('Digite o nome do Motorista; '))
+        if banco.checkMotorista(motorista):
+            print('O motorista já está cadastrado!!')
+        else:
+            break
+
+
+def buscarPlaca():
+    while True:
+        placa = str(input('Digite a placa que deseja encontrar: '))
+        if banco.checkPlaca(placa):
+            veiculo = banco.buscarPlaca(placa)
+            print(f'''
+            PLACA: {veiculo.get('placa')}
+            MOTORISTA: {veiculo.get('motorista')}
+            TIPO: {veiculo.get('tipo')}
+            ''')
+        else:
+            break
+
+
+def motoristaVeiculo():
+    while True:
+        placa = str(input('digite a placa: '))
+        if banco.checkPlaca(placa):
+            motorista = str(input('Digite o motorista que quer adicionar a placa: '))
+            banco.motoristaVeiculo(placa, motorista)
+            break
+        else:
+            print('Digite A placa corretamente !!')
+
+
+def removerMotorista():
+    while True:
+        placa = str(input('digite a placa: '))
+        if banco.checkPlaca(placa):
+            banco.removerMotorista(placa)
+            break
+        else:
+            print('Digite A placa corretamente !!')
+
+def veiculosEmotoristas():
+
+        veiculos = banco.bancoveiculo()
+        for a in veiculos.values():
+            if a.get('motorista')!= '':
+                print(f'''
+                Placa: {a.get ('placa')}
+                motorista: {a.get ('motorista')}
+                Tipo: {a.get ('tipo')}
+
+
+            ''')
+
+def motoristasSveiculos():
+    veiculos = banco.bancoveiculo()
+    for a in veiculos.values():
+        if a.get('motorista') != '':
+            print(f'''
+                    
+                    motorista: {a.get('motorista')}
+
+                ''')
+
+
+
+def removerVeiculo():
+    while True:
+        placa = str(input('digite a placa: '))
+        if banco.checkPlaca(placa):
+            banco.removerVeiculo(placa)
+            break
+        else:
+            print('Digite A placa corretamente !!')
+
+main()
+=======
 import Menus
 import veiculoBanco
 banco = veiculoBanco
@@ -58,3 +182,4 @@ def motoristaVeiculo():
 
 
 main()
+>>>>>>> 8a8063cdedaad4309bea0e1ffcacc058cf5bd1cf
